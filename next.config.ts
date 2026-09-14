fb7235e83238b7da69a4043f05c5dd75392c12f8
vaibhav sarkar <vaibhav.sarkar@apilayer.com>
2026-09-14 16:19:48 +0530

    added dynamic path in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import path from "path";
+
+const releaseDir = process.env.RELEASE_DIR || process.cwd();
+const releaseName = path.basename(releaseDir);
+
+const basePath = `/devtools/global-rates/${releaseName}`;
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath,
+  assetPrefix: basePath,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
