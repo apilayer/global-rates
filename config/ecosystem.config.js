8c9adfa225990f1cbf614efc5aeeb09e70acf01b
vaibhav sarkar <vaibhav.sarkar@apilayer.com>
2026-09-15 08:15:02 +0530

    workflow added

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [
     {
-      name: "global-rates",
-      cwd: process.env.RELEASE_DIR,
+      name: "globalrates",
+      cwd: "/var/www/devtools/global-rates",
       script: "npm",
       args: "start -- -p 3004",
       instances: 1,
